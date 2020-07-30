7ede17ec34dcab5e71c1d8be33f08af17746491b
vagrant <vagrant@localhost.localdomain>
2020-07-30 17:09:34 +0000

    [Fix]環境変数の記述変更

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -4,7 +4,7 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(
-      to: ENV['MAIL_ADDRESS'],
+      to: ENV.fetch("MAIL_ADDRESS"),
       subject: 'お問い合わせ通知'
     ) 
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,8 +62,8 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
   domain:               'gmail.com',
-  user_name:             ENV['MAIL_ADDRESS'],
-  password:              ENV['MAIL_PASSWORD'],
+  user_name:             ENV.fetch("MAIL_ADDRESS"),
+  password:              ENV.fetch("MAIL_PASSWORD"),
   authentication:       'plain',
   enable_starttls_auto:  true
 }
