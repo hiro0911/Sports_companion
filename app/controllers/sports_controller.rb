5ea9907ba1640fc22e8d68a35eec33228602dc31
vagrant <vagrant@localhost.localdomain>
2020-08-05 20:33:03 +0000

    [Add]不適切な画像ブロック機能追加

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -12,8 +12,27 @@ class SportsController < ApplicationController
 
   def update
     @sport = Sport.find(params[:id])
-    @sport.update(sport_params)
-    redirect_to sports_path(@sport.id)
+    if @sport.update(sport_params)
+      # google_apiに画像の値を渡す
+      unless @sport.image.blank?
+        visions = Vision.get_image_data(@sport.image)
+
+      # 受け取ったデータの配列を排出
+        visions.each do |vision|
+      # 不適切な画像である場合が高い又は非常に高い場合ブロック
+          if vision.include?("VERY_LIKELY") || vision.include?("LIKELY")
+            flash[:notice] = "不適切な画像の為ブロックされました。" 
+            @sport.image = nil
+            @sport.save
+            return render "edit"
+          end
+        end
+      end
+      path = Rails.application.routes.recognize_path(request.referer)
+      redirect_to path, notice: "スポーツをジャンル更新しました。"
+    else
+      render "edit" 
+    end
   end
 
   def new
@@ -23,9 +42,24 @@ class SportsController < ApplicationController
   def create
     @sport = Sport.new(sport_params)
     if @sport.save
-      redirect_to sports_path
+      # google_apiに画像の値を渡す
+      unless @sport.image.blank?
+        visions = Vision.get_image_data(@sport.image)
+
+      # 受け取ったデータの配列を排出
+        visions.each do |vision|
+      # 不適切な画像である場合が高い又は非常に高い場合ブロック
+          if vision.include?("VERY_LIKELY") || vision.include?("LIKELY")
+            flash[:notice] = "不適切な画像の為ブロックされました。" 
+            @sport.destroy
+            return render "new"
+          end
+        end
+      end
+      path = Rails.application.routes.recognize_path(request.referer)
+      redirect_to path, notice: "スポーツジャンルを作成しました。"
     else
-      render "new"
+      render "new" 
     end
   end
 
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -26,8 +26,22 @@ class TeamsController < ApplicationController
   def update
     @team = Team.find(params[:id])
     if @team.update(team_params)
-      path = Rails.application.routes.recognize_path(request.referer)
-      redirect_to path, notice: "チーム情報を更新しました。"
+      # google_apiに画像の値を渡す
+      unless @team.image.blank?
+        visions = Vision.get_image_data(@team.image)
+
+      # 受け取ったデータの配列を排出
+        visions.each do |vision|
+      # 不適切な画像である場合が高い又は非常に高い場合ブロック
+          if vision.include?("VERY_LIKELY") || vision.include?("LIKELY")
+            flash[:notice] = "不適切な画像の為ブロックされました。" 
+            @team.image = nil
+            @team.save
+            return render "edit"
+          end
+        end
+      end
+      redirect_to team_path(@team), notice: "チーム情報を更新しました。"
     else
       render "edit"
     end
@@ -38,53 +52,53 @@ class TeamsController < ApplicationController
     if @team.save
       # google_apiに画像の値を渡す
       unless @team.image.blank?
-       visions = Vision.get_image_data(@team.image)
+        visions = Vision.get_image_data(@team.image)
 
       # 受け取ったデータの配列を排出
-      visions.each do |vision|
+        visions.each do |vision|
       # 不適切な画像である場合が高い又は非常に高い場合ブロック
-        if vision.include?("VERY_LIKELY") || vision.include?("LIKELY")
-          flash[:notice] = "不適切な画像の為ブロックされました。" 
-          @team.destroy
-          return render "new"
+          if vision.include?("VERY_LIKELY") || vision.include?("LIKELY")
+            flash[:notice] = "不適切な画像の為ブロックされました。" 
+            @team.destroy
+            return render "new"
+          end
         end
       end
+      TeamCommentRoom.create(team_id: @team.id, id: @team.id)
+      TeamMember.create(user_id: current_user.id, team_id: @team.id)
+      redirect_to prefecture_sport_teams_path, notice: "チームを作成しました。"
+    else
+      render "new"
     end
-   TeamCommentRoom.create!(team_id: @team.id, id: @team.id)
-   TeamMember.create!(user_id: current_user.id, team_id: @team.id)
-   redirect_to prefecture_sport_teams_path, notice: "チームを作成しました。"
-  else
-   render "new"
   end
-end
 
-def information
-  @team = Team.find(params[:id])
-  @team_comment_room = TeamCommentRoom.find_by(team_id: @team.id)
-  unless @team.users.include?(current_user)
-    redirect_to "/", notice: "権限がありません。"
+  def information
+    @team = Team.find(params[:id])
+    @team_comment_room = TeamCommentRoom.find_by(team_id: @team.id)
+    unless @team.users.include?(current_user)
+      redirect_to "/", notice: "権限がありません。"
+    end
   end
-end
 
-def destroy
-  @team = Team.find(params[:id])
-  @team.destroy
-  redirect_to prefecture_sport_teams_path(prefecture_id: @team.prefecture_id, sport_id: @team.sport_id), notice: "チームを削除しました。"
-end
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to prefecture_sport_teams_path(prefecture_id: @team.prefecture_id, sport_id: @team.sport_id), notice: "チームを削除しました。"
+  end
 
-private
+  private
 
-def team_params
-  params.require(:team).permit(:sport_id,
-   :prefecture_id,
-   :name,
-   :image,
-   :applicant,
-   :area,
-   :team_name,
-   :introduction,
-   :stance,
-   :explanation,
-   :information)
-end
+  def team_params
+    params.require(:team).permit(:sport_id,
+     :prefecture_id,
+     :name,
+     :image,
+     :applicant,
+     :area,
+     :team_name,
+     :introduction,
+     :stance,
+     :explanation,
+     :information)
+  end
 end
